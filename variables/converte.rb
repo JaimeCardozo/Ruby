b44ef14ed6fd07e80ce7d45a1aeaead55b784c94
JaimeCardozo <jecardozo@uninorte.edu.co>
2022-12-19 00:20:07 -0500

    second comentary

---
+++ variables/converte.rb
@@ -0,0 +1,6 @@
+#this program convert string to integer_1
+puts "to write your number"
+number = gets
+number = number .to_i
+sum = number + 2
+puts sum
--- variables/read.rb
+++ variables/read.rb
@@ -1,3 +1,4 @@
+#this code aswer  your name (used )
 puts "What's your name? "
 name = gets
 puts "your name is #{name}"
