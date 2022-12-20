5886caa35fbdd42ae78a389f95ccd0406e00a8af
JaimeCardozo <jecardozo@uninorte.edu.co>
2022-12-20 18:12:16 -0500

    go!

--- variables/faces.rb
+++ variables/faces.rb
@@ -1,1 +1,6 @@
 #this code changes :) :( in your emoticons
+print("write me ")
+msg = gets
+msg = msg.gsub(":)","\u{1F603}")
+msg = msg.gsub(":(","\u{1F61E}")
+print(msg)
--- variables/usuario.rb
+++ variables/usuario.rb
@@ -1,4 +1,4 @@
-print("write your user with @")
+print("write your user with @ ")
 user = gets
 user = user.gsub("@", "@gmail.com")
 print("your user is #{user}")
