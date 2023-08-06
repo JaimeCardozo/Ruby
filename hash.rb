bd9257eba5977c7db5b8296971395a611a7934cd
Jaime Cardozo <jaime.cardozo172@gmail.com>
2023-08-05 20:34:48 -0500

    hash_Update_User

--- hash.rb
+++ hash.rb
@@ -28,20 +28,43 @@ def login(users, key, password)
 
 end
 def update_user(user, option)
-    case Option
+    case option
        when "1"
         "Change user name"
+        print "Digit the new user_name: "
+        user_name = gets.chomp
+        user[:user_name] = user_name
+        puts "The new name is: #{user_name}"
        when "2"
         "Change password"
+        print "Digit the new password: "
+        password = gets.chomp
+        user[:password] = password
+        puts "The new password is: #{password}"
        when "3"
         "Change favorite game"
+        print "Digit your new favorite game: "
+        fav_games = gets.chomp
+        games = user[:games]
+        games = games.push(games[0])
+        games[0] = fav_games
+        puts "The new favorite game is: #{fav_games}"
+       else
+        print "This option not exist"
     end
+    return user
 end
 def main()
     users = {}
-    while true
+    sw = true
+    while sw
         puts "The menu"
-        puts "What option want: 1 = Created user, 2 = View user information, 3 = Update some user, 4 = Delete user"
+        puts "What option you want: 
+        1 = Created user 
+        2 = View user information
+        3 = Create Adminitrate
+        4 = Power Gem
+        5= Exit "
         option = gets.chomp
           case option
           when "1"
@@ -65,24 +88,39 @@ def main()
                   print "Digit your password: "
                   password = gets.chomp
                   if login(users, key, password)
-                      print "What you wish view: 1. Information general, 2. update information_user, 
-                      3. Add new game, 4. Erased user, 5.Login_Exit"
+                      login = true 
+                      while login
+                      puts "What you wish view: 
+                      1. Information general
+                      2. update information_user 
+                      3. Add new game 
+                      4. Erased user 
+                      5.Login_Exit "
                       opt = gets.chomp
                       case opt
                        when "1"
+                        "View user"
                         puts users[key]
                        when "2"
+                        "User_Update"
                         puts "You only change: user_name, password, favorite_game "
                         puts "Digit option: 1 = user_name, 2 = password, 3 = favorite_game"
                         opt2 = gets.chomp
-                        update_user(users[key],opt2)
+                        users[key] = update_user(users[key],opt2)
                        when "3"
                         "Add new game"
+                        print "Digit the name of new games: "
+                        game = gets.chomp
+                        user = users[key]
+                        user[:games]= user[:games].push(game)
                        when "4"
                         "Erased user"
                        when "5"
                         "Login_Exit"
+                        login = false 
+                        puts "The login was close"
                       end
+                    end
                   else
                       puts "Login Again"
                   end
@@ -91,9 +129,12 @@ def main()
                   "Create adminitrater"
                   
           when "4"
+            "Power_Gem"
                   "Adminitrater, Here you update some user: "
                   "Here you delete some user"
                   "Here you delete all user"
+          when "5"
+            sw = false
           else
                  puts "the opion is not valid"
           end
