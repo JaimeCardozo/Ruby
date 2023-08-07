316637ec6bc4adef6028db7d497912079892e45d
Jaime Cardozo <jaime.cardozo172@gmail.com>
2023-08-07 18:50:57 -0500

    administrator_hash

--- hash.rb
+++ hash.rb
@@ -17,6 +17,7 @@ def login(users, key, password)
     if users.has_key?(key)
         user = users[key]
         if user[:password] == password
+            puts "Welcome! #{user[:user_name]}"
             return true
         else
             puts "This Password is not correct"
@@ -54,8 +55,22 @@ def update_user(user, option)
     end
     return user
 end
+def create_administrater()
+    print "Digit your Password: "
+    password = gets.chomp
+    print "Digit your User_name: "
+    user_name = gets.chomp
+    print "Digit your favorite_girl_xd: "
+    favorite_girl = gets.chomp 
+    administrater_infor = {
+        :user_name=> user_name,
+        :password=> password, 
+        :girl => favorite_girl}   
+    return administrater_infor
+end
 def main()
     users = {}
+    administraters = {}
     sw = true
     while sw
         puts "The menu"
@@ -127,12 +142,78 @@ def main()
       
           when "3"
                   "Create adminitrater"
-                  
+                  puts "In this moment you create a new administrater"
+                  print "Please, Write the administrater_key: "
+                  administrater_key = gets.chomp
+                  key = administrater_key.to_sym
+                  if administraters.has_key?(key)
+                  puts "This administrater exist, please write other administrater" 
+                  else
+                  administraters[key] = create_administrater()
+                  puts "the administrater was created, this is: "
+                  puts administraters[key]
+                  end
           when "4"
             "Power_Gem"
                   "Adminitrater, Here you update some user: "
                   "Here you delete some user"
                   "Here you delete all user"
+                print "Writed administrater_key: "
+                key = gets.chomp.to_sym
+                print "Writed password: "
+                password = gets.chomp
+                if login(administraters, key, password)
+                    sw0 = true
+                while sw0
+                  puts "What do you wish? 1 = View information user, 2 = erased all users, 3 = mod_happy"
+                  opt = gets.chomp
+                  case opt
+                  when "1"
+                        print " Digit the email of user, you wish review: "
+                        key = gets.chomp.to_sym
+                        if users.has_key?(key)
+                            sw2 = true
+                            while sw2 
+                            print "What wish with this user: 
+                            1 = View user, 
+                            2 = erased user, 
+                            3 = update user, 
+                            4 = User_Exit"
+                            opt2 = gets.chomp
+                            case opt2
+                                when "1"
+                                  puts "The user is: "
+                                  print users[key]
+                                when "2"
+                                    "erased user"
+                                when "3"
+                                   puts "You only change: user_name, password, favorite_game "
+                                   puts "Digit option: 1 = user_name, 2 = password, 3 = favorite_game"
+                                   opt2 = gets.chomp
+                                   users[key] = update_user(users[key],opt2)
+                                when "4"
+                                    sw2 = false
+                                else
+                                    print "This option is not valid"
+                            end
+                        end
+                            
+                        else
+                            puts "This email not exist"
+                        end
+                    when "2"
+                        "erased all users"
+                    when "3"
+                        sw0 = false
+                    else
+                        puts "this option is not valid"
+
+                  end
+                end
+
+                else
+                    puts "Error in login, again_login"
+                end
           when "5"
             sw = false
           else
