57b6b9f7264a14599b73720117a1767f21529147
Jaime Cardozo <jaime.cardozo172@gmail.com>
2023-08-08 18:59:32 -0500

    methods_delete

--- test2.rb
+++ test2.rb
@@ -1,6 +1,20 @@
 #methods keys: said keys of hash 
 edades = {maria: 12, jose: 13, jaime: 18}
 print edades.keys
-#methods store add some element to hash
+puts
+#methods store: add some element to hash
 edades.store(:joselito, 20)
 print edades
+puts
+#methods values: return of values of hash
+print edades.values
+puts
+#methods hash_value?: return true o false if exist a value in the hash
+puts edades.has_value?(20)
+puts edades.has_value?(21) 
+#methods delete: this method is for delete element of hash
+key = edades.delete(:maria)
+puts key
+puts edades
+edades = {}
+puts edades
