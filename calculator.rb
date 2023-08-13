1ec66cc27ed49186479db3cc9f722ed3e59faae1
Jaime Cardozo <jaime.cardozo172@gmail.com>
2023-08-13 18:03:23 -0500

    calculator_file

--- calculator.rb
+++ calculator.rb
@@ -1,41 +1,74 @@
+def get_number()
+    print "Digit your number: "
+    number = gets.chomp.to_i
+    return number 
+end
+def give_number()
+
+end
 def main 
     puts "Welcome a your calculator"
     if !File.exists?("operations.txt")
        operations = File.new("operations.txt","w+")
-    else
-       operations = File.open("operations.txt")
     end
-    
     loop do
+        operations = File.open("operations.txt","a+")
         puts "the operation valid are: 
         1 = Adittion
         2 = Subtraction
         3 = Multiplication
         4 = Division
-        5 = Exit"
-        opt = gets.chomp
-        if opt == "5"
+        5 = View operations 
+        6 = Exit
+        7 = Delete_Operations"
+        opt = gets.chomp.to_i
+        if opt == "6"
             break
         end
-        puts "Digit the first number? "
-        number1 = gets.chomp
-        puts "Digit the second number? "
-        number2 = gets.chomp
         case opt
-        when "1"
-            result = number1 + number2 
-        when "2"
+        when 1
+            number1 = get_number()
+            number2 = get_number()
+            result = number1 + number2
+            line = "#{number1} + #{number2} = #{result}\n" 
+        when 2
+            number1 = get_number()
+            number2 = get_number()
             result = number1 - number2
-        when "3"
+            line = "#{number1} - #{number2} = #{result}\n"
+        when 3
+            number1 = get_number()
+            number2 = get_number()
             result = number1 * number2
-        when "4"
+            line = "#{number1} * #{number2} = #{result}\n"
+        when 4
+            number1 = get_number()
+            number2 = get_number()
             result = number1 / number2
+            line = "#{number1} / #{number2} = #{result}\n"
+        when 5
+            puts "read the operations"
+            File.open("operations.txt").each do |line|
+                puts line
+            end
+        when 6
+            break
+        when 7
+           puts "delete operations"
+           operations.close
+           File.delete("operations.txt")
+           operations = File.new("operations.txt")
         else 
             puts "Option invalid"
         end
-        puts result
-
+        if opt == 1 or opt == 2 or opt == 3 or opt == 4
+            puts " the result is: #{result}"
+            puts line
+            operations.write(line)
+            operations.close
+        end
+        
     end
-    
+    operations.close
 end
 main()
--- converte.rb
+++ converte.rb
@@ -1,6 +1,7 @@
 #this program convert string to integer_1
 puts "to write your number"
-number = gets
-number = number .to_i
+number = gets.chomp
+number = number.to_i
 sum = number + 2
 puts sum
+puts number.to_s
